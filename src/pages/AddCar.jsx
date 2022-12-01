94e13d71d45d61560244c7ff9d9e27d676835ccb
Tochukwu Orizu <orizut@gmail.com>
2022-12-01 12:22:07 -0800

    Add containers for navbar

---
+++ src/pages/AddCar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddCar = () => (
+  <div>AddCar</div>
+);
+
+export default AddCar;
---
+++ src/pages/MyReservations.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyReservations = () => (
+  <div>MyReservations</div>
+);
+
+export default MyReservations;
