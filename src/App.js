a195d6dcd3eb92dfa7b829238589701361639601
Tochukwu Orizu <orizut@gmail.com>
2022-12-01 13:34:33 -0800

    Add menueopener

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Splash from './pages/Splash';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Splash />} />
       </Routes>
