97ab307e97e3b26288d5583b344899d90aaa9093
Tochukwu Orizu <orizut@gmail.com>
2022-12-01 13:45:30 -0800

    Add props for menueopener

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import MenuOpener from './components/MenuOpener';
 import Navbar from './components/Navbar';
 import Splash from './pages/Splash';
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const toggleMenu = () => {
+    setOpen(!open);
+  };
+
   return (
     <div>
-      <Navbar />
+      <MenuOpener open={open} toggleMenu={toggleMenu} />
+      <Navbar open={open} />
       <Routes>
         <Route path="/" element={<Splash />} />
       </Routes>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Footer from './Footer';
 import Logo from './Logo';
 
-const Navbar = () => (
-  <nav>
-    <Logo />
-    <ul>
-      <li><NavLink to="cars">Cars</NavLink></li>
-      <li><NavLink to="reserve">Reserve</NavLink></li>
-      <li><NavLink to="my_reservations">My Reservations</NavLink></li>
-      <li><NavLink to="add_car">Add Car</NavLink></li>
-    </ul>
-    <Footer />
-  </nav>
-);
+const Navbar = (props) => {
+  const { open } = props;
+  return (
+    <nav className={open ? '' : '-translate-x-48'}>
+      <Logo />
+      <ul>
+        <li><NavLink to="cars">Cars</NavLink></li>
+        <li><NavLink to="reserve">Reserve</NavLink></li>
+        <li><NavLink to="my_reservations">My Reservations</NavLink></li>
+        <li><NavLink to="add_car">Add Car</NavLink></li>
+      </ul>
+      <Footer />
+    </nav>
+  );
+};
+
+Navbar.propTypes = {
+  open: PropTypes.bool.isRequired,
+};
 
 export default Navbar;
