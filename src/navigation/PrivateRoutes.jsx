ec37307d6ad14ab69673c18611827f0363bf54fa
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-12-02 23:30:08 +0430

    Check if the user is authenticated

--- src/navigation/PrivateRoutes.jsx
+++ src/navigation/PrivateRoutes.jsx
@@ -4,6 +4,8 @@ export default function PrivateRoutes({ children, requiresAdmin }) {
   const { user, status } = useSelector((state) => state.user);
   const isAdmin = user && user.role === 'admin';
 
+  const isAuthenticated = user && user.id;
+
 }
 
 
