748f7f112e868f438f3faa0c45572c89370f95ec
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-12-02 22:04:43 +0430

    Hundle errors and user success status

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useLocation } from 'react-router-dom';
 import logInUser from '../../redux/actions/User/logInUser';
 
 export default function LogIn() {
+  const [errorMessage, setErrorMessage] = useState('');
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const formRef = useRef();
+  const { location } = useLocation();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,6 +21,20 @@ export default function LogIn() {
     };
     dispatch(logInUser(userInfo));
   };
+
+  useEffect(() => {
+    if (user.status === 'success') {
+      try {
+        if (user.user.error) {
+          setErrorMessage(user.user.error);
+        } else {
+          navigate(location ? location.from : '/cars');
+        }
+      } catch (e) {
+        setErrorMessage(e.error);
+      }
+    }
+  }, [navigate, user, location, setErrorMessage]);
 
   return (
     <form ref={formRef} className="add-form" onSubmit={handleSubmit}>
