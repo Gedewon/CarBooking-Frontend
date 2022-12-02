bf2ab71c67ab9a21252090f800dd1a980d827ad0
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-12-02 21:06:29 +0430

    Add cars and reservation server URLs

--- src/navigation/routes.jsx
+++ src/navigation/routes.jsx
@@ -2,3 +2,5 @@ const BASE_URL = 'https://carbackend.onrender.com';
 const LOG_IN = '/api/v1/auth/sign_in';
 const SIGN_UP = '/api/v1/auth/sign_up';
 const LOG_OUT = '/api/v1/auth/sign_out';
+const CARS = '/api/v1/cars';
+const RESERVATION = '/api/v1/reservations';
