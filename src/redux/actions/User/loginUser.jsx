cf28a90a6f31134c96aa6c10b5f69d8a026e8303
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-12-02 21:16:04 +0430

    Write code to make request for login access with token

--- src/redux/actions/User/loginUser.jsx
+++ src/redux/actions/User/loginUser.jsx
@@ -1,7 +1,16 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { BASE_URL, LOG_IN } from '../../../navigation/routes';
 
-const logInUser = createAsyncThunk('LOG_IN', async () => {
 
+const logInUser = createAsyncThunk('LOG_IN', async (userinfo) => {
+  const response = await fetch(`${BASE_URL + LOG_IN}`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      accept: 'application/json',
+    },
+    body: JSON.stringify(userinfo),
+  });
 });
 
 export default logInUser;
