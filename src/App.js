653ca07440092aa597837ec7d58a5b3b4606fb6e
Tochukwu Orizu <orizut@gmail.com>
2022-12-02 00:14:15 -0800

    Create route for add form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
-import Reservation from './pages/Reservation';
 import Splash from './pages/Splash';
+import AddCardForm from './pages/add_car/AddCardForm';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Splash />} />
-        <Route path="/reservation" element={<Reservation />} />
+        <Route path="/add/car" element={<AddCardForm />} />
       </Routes>
     </div>
   );
