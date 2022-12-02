2b2a788c58897ac216b4db3ab02ac768781f1032
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-12-02 21:07:10 +0430

    export URLs

--- src/navigation/routes.jsx
+++ src/navigation/routes.jsx
@@ -4,3 +4,7 @@ const SIGN_UP = '/api/v1/auth/sign_up';
 const LOG_OUT = '/api/v1/auth/sign_out';
 const CARS = '/api/v1/cars';
 const RESERVATION = '/api/v1/reservations';
+
+export {
+  BASE_URL, LOG_IN, LOG_OUT, SIGN_UP, CARS, RESERVATION
+};
