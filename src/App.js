7f418e19f786bc1ee5bf14e6c6c01583a7765494
Tochukwu Orizu <orizut@gmail.com>
2022-12-03 10:34:26 -0800

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -8,22 +8,12 @@ function App() {
   const [open, setOpen] = useState(false);
   const location = useLocation();
 
-  const location = useLocation();
-
   const toggleMenu = () => {
     setOpen(!open);
   };
 
   return (
     <div>
-      { (location.pathname !== '/' && location.pathname !== '/sign_up')
-        && (
-        <>
-          <MenuOpener open={open} toggleMenu={toggleMenu} />
-          <Navbar open={open} />
-        </>
-        )}
-      <RouterConfig />
       { (location.pathname !== '/' && location.pathname !== '/sign_up')
         && (
         <>
