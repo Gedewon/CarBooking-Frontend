77b3f63334c64d22d754e1028c81988b716251ef
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-12-03 11:03:18 +0430

    Filter cars

--- src/pages/Reserve.jsx
+++ src/pages/Reserve.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 function Reservation() {
   const [ReserveOpener, setReserveOpener] = useState(true);
   const { cars } = useSelector((state) => state.cars);
+  const dispatch = useDispatch();
+
+  const { id } = useParams();
+
+  const car = cars.filter((car) => car.id === Number(id))[0];
 
   return (
     <section>
