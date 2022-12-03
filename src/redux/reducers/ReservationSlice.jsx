c54332a093de2307efff7028dc37598346f7258d
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-12-03 09:34:51 +0430

    Hundle fullfilled dispatched action for reservation fetch

--- src/redux/reducers/ReservationSlice.jsx
+++ src/redux/reducers/ReservationSlice.jsx
@@ -1,4 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
+import getReservations from '../actions/Reservation/getReservation';
+import addReservations from '../actions/Reservation/addReservation';
 
 const initialState = {
   status: 'idle',
@@ -9,6 +11,14 @@ const reservationSlice = createSlice({
   name: 'reservation',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getReservations.fulfilled, (state, action) => {
+        state.status = 'success';
+        state.reservation.push(action.payload);
+      })
+  },
 });
 
+export const reservationsRedux = reservationSlice.actions;
 export default reservationSlice.reducer;
