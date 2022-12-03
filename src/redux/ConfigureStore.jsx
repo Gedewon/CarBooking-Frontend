24ddeb10ed76734fffdbb42dfe675da35514c7b9
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-12-03 09:54:15 +0430

    Import reducers

--- src/redux/ConfigureStore.jsx
+++ src/redux/ConfigureStore.jsx
@@ -1,6 +1,8 @@
-import { combineReducers } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import userSlice from './reducers/UserSlice';
+import carSlice from './reducers/CarSlice';
+import ReservationSlice from './reducers/ReservationSlice';
 
 const rootReducer = combineReducers({
   user: userSlice,
