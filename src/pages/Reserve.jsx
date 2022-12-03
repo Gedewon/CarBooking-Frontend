466c12a46524dc5236da0498472d2e1c4ae947a9
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-12-03 11:07:36 +0430

    Render conditionaly CarDetail and Loading components as child and passing data as props

--- src/pages/Reserve.jsx
+++ src/pages/Reserve.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import Loading from '../../components/Buttons/Loading';
+import CarDetail from '../../components/Cars/CarDetail';
 
 function Reservation() {
   const [ReserveOpener, setReserveOpener] = useState(true);
@@ -16,7 +18,25 @@ function Reservation() {
   const car = cars.filter((car) => car.id === Number(id))[0];
 
   return (
-    <section>
+    <section >
+      {car && car.id ? (
+        <>
+          <img
+            src={car.image}
+            alt={car.name}
+          />
+          <CarDetail
+            id={Number(id)}
+            name={car.name}
+            carType={car.car_type}
+            carBrand={car.brand}
+            carPrice={car.fee_per_day}
+            carColor={car.color}
+            toggleReservation={toggleReservation}
+            ReserveOpener={ReserveOpener}
+          />
+        </>
+      ) : <Loading message="Loading Car information..." /> }
     </section>
   );
 }
