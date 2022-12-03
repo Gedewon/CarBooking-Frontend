6fe923d460cccd6be85134b40dba4a0392042513
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-12-03 12:17:59 +0430

    Add jsx to Reserve component

--- src/pages/Reserve.jsx
+++ src/pages/Reserve.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 
-const Reserve = () => (
-  <div>Reserve</div>
-);
+function Reserve() {
+  return (
+    <div>
+      <div>
+        <h1>Reserve Cars.</h1>
+        <p>
+          Please Select a car to reserve form the provided lists.
+        </p>
+      </div>
+      <form>
+        <h3>
+          Which Car do you want to reserve?
+        </h3>
+        <button type="submit">
+          Reserve Car
+        </button>
+      </form>
+    </div>
+  );
+}
 
 export default Reserve;
-
