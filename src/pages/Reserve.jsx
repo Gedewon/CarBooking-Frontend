d4e3d1fccebfb4eaf5831ad4a7054382edc3ce94
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-12-03 12:20:42 +0430

    Add style

--- src/pages/Reserve.jsx
+++ src/pages/Reserve.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 
 function Reserve() {
+
   return (
-    <div>
-      <div>
-        <h1>Reserve Cars.</h1>
-        <p>
+    <div className="pb-8">
+      <div className="flex flex-col items-center justify-center py-16 ">
+        <h1 className=" text-4xl font-bold text-center">Reserve Cars.</h1>
+        <p className=" text-xs text-slate-400">
           Please Select a car to reserve form the provided lists.
         </p>
       </div>
-      <form>
-        <h3>
+      <form
+        className=" px-8 flex flex-col gap-4"
+      >
+        <h3 className="mb-5 text-lg font-medium text-gray-900 dark:text-white">
           Which Car do you want to reserve?
         </h3>
-        <button type="submit">
+        <button type="submit" className="submit-button self-center">
           Reserve Car
         </button>
       </form>
