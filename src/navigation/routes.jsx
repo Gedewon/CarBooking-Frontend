23bf24a7c757875f3c499c13a202c2e29fc6aaa1
gedewon <gedewon.hayle.9@gmail.com>
2022-12-04 20:48:20 +0300

    Fix token validation logic

--- src/navigation/routes.jsx
+++ src/navigation/routes.jsx
@@ -2,6 +2,7 @@ const BASE_URL = 'http://localhost:3000/';
 // const BASE_URL = 'http://localhost:3000';
 const IMAGE_URL = 'https://ik.imagekit.io/carbackend/';
 const LOG_IN = 'api/v1/auth/sign_in';
+const VALIDATE_TOKEN = 'api/v1/auth/validate_token';
 const LOG_OUT = 'api/v1/auth/sign_out';
 const SIGN_UP = 'api/v1/auth/sign_up';
 const CARS = 'api/v1/cars';
@@ -10,5 +11,5 @@ const IMAGE_AUTH = 'api/v1/auth/imagekit';
 const IMAGE_KEY = 'public_FbHvKLKUQasD0G/tcdKYjl+MyO4=';
 
 export {
-  BASE_URL, LOG_IN, LOG_OUT, SIGN_UP, CARS, RESERVATION, IMAGE_URL, IMAGE_AUTH, IMAGE_KEY,
+  BASE_URL, LOG_IN, LOG_OUT, SIGN_UP, CARS, RESERVATION, IMAGE_URL, IMAGE_AUTH, IMAGE_KEY,VALIDATE_TOKEN
 };
